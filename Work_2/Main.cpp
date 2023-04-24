623ee21f0ca110e2159012b6eba90724b83ff5b1
Danila <alexseev_dnl@mail.ru>
2023-04-24 20:39:25 +0300

    qwr

--- Work_2/Main.cpp
+++ Work_2/Main.cpp
@@ -1,15 +1,6 @@
-/*
-Для начала нужно нажать enter
-кнопки 1 и 2 направляют соответствующий куб в обратное направление (у каждого нажатия есть кд, так как без этого кнопка считывается на многих тактах)
-при столкновении можно опять запустить через enter
-*/
-
-
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include<cmath>
-//#include <gl/gl.h>
 
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
@@ -18,6 +9,13 @@
 #include "Shaders.h"
 #include "Camera.h"
 #include "stb_image.h"
+
+#define CUBS_COUNT 2
+#define SPHERE_COUNT 6
+
+
+
+float xR = 0.001f, yR = 0.001f, zR = 0.001f;
 
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
@@ -68,6 +66,11 @@ void processInput(GLFWwindow* window, glm::vec3* cubeRotation, size_t& taktFlag)
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 unsigned int loadTexture(const char* path);
+void boxMove(glm::vec3& cubePositions, glm::vec3& cubeRotation, glm::vec3* cubeLightPos);
+void sphereMove(glm::vec3& spherePositions, glm::vec3& sphereRotation, glm::vec3* cubeLightPos, float* sphereSize, size_t sphereNumber);
+bool boxCollision(glm::vec3* cubePositions, size_t curCubeId, glm::vec3* cubeRotation, glm::vec3* spherePositions, glm::vec3* sphereRotation, float* sphereSize);
+bool sphereCollision(glm::vec3* spherePositions, size_t curSphereId, glm::vec3* sphereRotation, float* sphereSize);
+void ChangeDierction(glm::vec3* cubeRotation, size_t cubeNumber);
 //void Map_Init();
 //void Map_Show();
 
@@ -153,17 +156,21 @@ int main()
 		glm::vec3(-1.1f,  1.7f, 1.0f),
 		glm::vec3(-1.0f, 2.0f, -2.5f),
 		glm::vec3(-3.0f, -2.5f, 2.3f),
-		glm::vec3(2.4f, 0.9f, 3.5f),
-		glm::vec3(1.2f,  2.0f, -1.9f),
+		glm::vec3(2.4f, 0.9f, -3.0f),
+		glm::vec3(1.2f,  -3.0f, -1.9f),
 		glm::vec3(0.6f, -0.7f, -2.5f),
 		glm::vec3(1.1f,  2.3f, -1.2f),
 		glm::vec3(0.5f,  0.2f, -1.5f),
 		glm::vec3(-1.7f,  1.5f, -1.0f)
 	};
 
+	float sphereSize[] = {
+		0.45f, 0.95f, 1.1f, 1.4f
+	};
+
 	glm::vec3 cubePositions[] = {
-		glm::vec3(0.0f,  0.0f,  0.0f),
-		glm::vec3(2.0f,  2.0f, 2.0f),
+		glm::vec3(0.0f,  0.0f,  1.0f),
+		glm::vec3(2.0f,  2.0f, 3.0f),
 		glm::vec3(-1.5f, -2.2f, -2.5f),
 		glm::vec3(-3.8f, -2.0f, -2.3f),
 		glm::vec3(2.4f, -0.4f, -3.5f),
@@ -174,17 +181,47 @@ int main()
 		glm::vec3(-1.3f,  1.0f, -1.5f)
 	};
 
-	glm::vec3 pointLightPositions[] = {
-		glm::vec3(0.7f,  0.2f,  2.0f),
-		glm::vec3(2.3f, -3.3f, -4.0f),
-		glm::vec3(-4.0f,  2.0f, -12.0f),
-		glm::vec3(0.0f,  0.0f, -3.0f)
+	glm::vec3 cubeLightPos[] = {
+		glm::vec3(4.5f, 3.5f, 4.0f),
+		glm::vec3(-4.5f, -3.5f, -4.0f),
+		glm::vec3(4.5f, 3.5f, -4.0f),
+		glm::vec3(4.5f, -3.5f, -4.0f),
+		glm::vec3(4.5f, -3.5f, 4.0f),
+		glm::vec3(-4.5f, -3.5f, 4.0f),
+		glm::vec3(-4.5f, 3.5f, -4.0f),
+		glm::vec3(-4.5f, 3.5f, 4.0f)
+	};
+
+	glm::vec3 cubeRotation[] = {
+		glm::vec3(0.0009f,  0.0009f,  0.0009f),
+		glm::vec3(0.0009f,  -0.0009f,  -0.0009f),
+		glm::vec3(0.0009f,  0.0009f,  0.0009f),
+		glm::vec3(0.0009f,  0.0009f,  0.0009f),
+		glm::vec3(0.0007f,  0.0007f,  0.0007f),
+		glm::vec3(0.0007f,  0.0007f,  0.0007f),
+		glm::vec3(0.0007f,  0.0007f,  0.0007f),
+		glm::vec3(0.0007f,  0.0007f,  0.0007f),
+		glm::vec3(0.0007f,  0.0007f,  0.0007f),
+		glm::vec3(0.0007f,  0.0007f,  0.0007f)
+	};
+
+	glm::vec3 sphereRotation[] = {
+		glm::vec3(0.0009f,  0.0009f,  -0.0009f),
+		glm::vec3(-0.0009f,  -0.0009f,  0.0009f),
+		glm::vec3(-0.0009f,  0.0009f,  0.0009f),
+		glm::vec3(-0.0009f,  -0.0009f,  -0.0009f),
+		glm::vec3(0.0009f,  0.0009f,  0.0009f),
+		glm::vec3(0.0009f,  0.0009f,  0.0009f),
+		glm::vec3(0.0009f,  0.0009f,  0.0009f),
+		glm::vec3(0.0009f,  0.0009f,  0.0009f),
+		glm::vec3(0.0009f,  0.0009f,  0.0009f),
+		glm::vec3(0.0009f,  0.0009f,  0.0009f)
 	};
 
 	std::vector<float> SphereVertices;
 	std::vector<int> SphereIndices;
 
-	for(int y = 0; y <= Y_segments; y++)
+	for (int y = 0; y <= Y_segments; y++)
 		for (int x = 0; x <= X_segments; x++)
 		{
 			float xSegment = (float)x / (float)X_segments;
@@ -197,15 +234,15 @@ int main()
 			SphereVertices.push_back(zPos);
 		}
 
-	for(int i = 0; i < Y_segments; i++)
+	for (int i = 0; i < Y_segments; i++)
 		for (int j = 0; j < X_segments; j++)
 		{
-			SphereIndices.push_back(i* (X_segments + 1) + j);
+			SphereIndices.push_back(i * (X_segments + 1) + j);
 			SphereIndices.push_back((i + 1) * (X_segments + 1) + j);
-			SphereIndices.push_back((i + 1)* (X_segments + 1) + j + 1);
-			SphereIndices.push_back(i* (X_segments + 1) + j);
-			SphereIndices.push_back((i + 1)* (X_segments + 1) + j + 1);
-			SphereIndices.push_back(i* (X_segments + 1) + j + 1);
+			SphereIndices.push_back((i + 1) * (X_segments + 1) + j + 1);
+			SphereIndices.push_back(i * (X_segments + 1) + j);
+			SphereIndices.push_back((i + 1) * (X_segments + 1) + j + 1);
+			SphereIndices.push_back(i * (X_segments + 1) + j + 1);
 		}
 
 	unsigned int VBO, cubeVAO, sphereVAO, sphereVBO;
@@ -231,7 +268,7 @@ int main()
 
 	glGenVertexArrays(1, &sphereVAO);
 	glGenBuffers(1, &sphereVBO);
-	
+
 	glBindBuffer(GL_ARRAY_BUFFER, sphereVBO);
 	glBufferData(GL_ARRAY_BUFFER, SphereVertices.size() * sizeof(float), &SphereVertices[0], GL_STATIC_DRAW);
 
@@ -274,7 +311,9 @@ int main()
 	sphereShader.setInt("material.diffuse", 0);
 	sphereShader.setInt("material.specular", 1);
 	//lightingShader.setInt("material.emission", 2);
-	
+
+	size_t taktFlag = 0;
+
 	while (!glfwWindowShouldClose(window))
 	{
 		if (glfwGetKey(window, GLFW_KEY_ENTER) == GLFW_PRESS) startGame = true;
@@ -331,7 +370,38 @@ int main()
 		lightingShader.setFloat("pointLights[3].linear", 0.09);
 		lightingShader.setFloat("pointLights[3].quadratic", 0.032);
 
-		// Прожектор
+		lightingShader.setVec3("pointLights[4].position", cubeLightPos[4]);
+		lightingShader.setVec3("pointLights[4].ambient", 0.05f, 0.05f, 0.05f);
+		lightingShader.setVec3("pointLights[4].diffuse", 0.8f, 0.8f, 0.8f);
+		lightingShader.setVec3("pointLights[4].specular", 1.0f, 1.0f, 1.0f);
+		lightingShader.setFloat("pointLights[4].constant", 1.0f);
+		lightingShader.setFloat("pointLights[4].linear", 0.09);
+		lightingShader.setFloat("pointLights[4].quadratic", 0.032);
+
+		lightingShader.setVec3("pointLights[5].position", cubeLightPos[5]);
+		lightingShader.setVec3("pointLights[5].ambient", 0.05f, 0.05f, 0.05f);
+		lightingShader.setVec3("pointLights[5].diffuse", 0.8f, 0.8f, 0.8f);
+		lightingShader.setVec3("pointLights[5].specular", 1.0f, 1.0f, 1.0f);
+		lightingShader.setFloat("pointLights[5].constant", 1.0f);
+		lightingShader.setFloat("pointLights[5].linear", 0.09);
+		lightingShader.setFloat("pointLights[5].quadratic", 0.032);
+
+		lightingShader.setVec3("pointLights[6].position", cubeLightPos[6]);
+		lightingShader.setVec3("pointLights[6].ambient", 0.05f, 0.05f, 0.05f);
+		lightingShader.setVec3("pointLights[6].diffuse", 0.8f, 0.8f, 0.8f);
+		lightingShader.setVec3("pointLights[6].specular", 1.0f, 1.0f, 1.0f);
+		lightingShader.setFloat("pointLights[6].constant", 1.0f);
+		lightingShader.setFloat("pointLights[6].linear", 0.09);
+		lightingShader.setFloat("pointLights[6].quadratic", 0.032);
+
+		lightingShader.setVec3("pointLights[7].position", cubeLightPos[7]);
+		lightingShader.setVec3("pointLights[7].ambient", 0.05f, 0.05f, 0.05f);
+		lightingShader.setVec3("pointLights[7].diffuse", 0.8f, 0.8f, 0.8f);
+		lightingShader.setVec3("pointLights[7].specular", 1.0f, 1.0f, 1.0f);
+		lightingShader.setFloat("pointLights[7].constant", 1.0f);
+		lightingShader.setFloat("pointLights[7].linear", 0.09);
+		lightingShader.setFloat("pointLights[7].quadratic", 0.032);
+
 		lightingShader.setVec3("spotLight.position", camera.Position);
 		lightingShader.setVec3("spotLight.direction", camera.Front);
 		lightingShader.setVec3("spotLight.ambient", 0.0f, 0.0f, 0.0f);
@@ -359,13 +429,24 @@ int main()
 		glActiveTexture(GL_TEXTURE1);
 		glBindTexture(GL_TEXTURE_2D, specularMap);
 
-		// рендеринг ящика
+		if (startGame) {
+			for (int i = 0; i < CUBS_COUNT; i++) {
+				boxCollision(cubePositions, i, cubeRotation, spherePositions, sphereRotation, sphereSize);
+				boxMove(cubePositions[i], cubeRotation[i], cubeLightPos);
+			}
+			for (int i = 0; i < SPHERE_COUNT; i++) {
+				sphereCollision(spherePositions, i, sphereRotation, sphereSize);
+				sphereMove(spherePositions[i], sphereRotation[i], cubeLightPos, sphereSize, i);
+			}
+		}
+
 		glBindVertexArray(cubeVAO);
 		for (unsigned int i = 0; i < CUBS_COUNT; i++)
 		{
 			glm::mat4 model = glm::mat4(1.0f);
 			model = glm::translate(model, cubePositions[i]);
-			float angle = 20.0f * i;
+			float angle = 20.0f * spin;
+
 			model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
 			lightingShader.setMat4("model", model);
 
@@ -378,11 +459,11 @@ int main()
 		{
 			glm::mat4 model_sphere = glm::mat4(1.0f);
 			glm::vec3 col1(1.0f, 1.0f, 1.0f);
-			if ((i + 1) % 2 == 0)
+			if (i <= 1)
 				col1 = glm::vec3(1.0f, 0.5f, 0.5f);
-			else if ((i + 1) % 3 == 0)
+			else if (i <= 3)
 				col1 = glm::vec3(0.5f, 1.0f, 0.5f);
-			else if ((i + 1) % 5 == 0)
+			else if (i <= 5)
 				col1 = glm::vec3(0.5f, 0.5f, 1.0f);
 			else
 				col1 = glm::vec3(1.0f, 0.45f, 0.0f);
@@ -396,14 +477,14 @@ int main()
 			glBindVertexArray(sphereVAO);
 			model_sphere = glm::mat4(1.0f);
 			model_sphere = glm::translate(model_sphere, spherePositions[i]);
-			if((i + 1) % 2 == 0)
-				model_sphere = glm::scale(model_sphere, glm::vec3(0.2f));
-			else if ((i + 1) % 3 == 0)
-				model_sphere = glm::scale(model_sphere, glm::vec3(0.6f));
-			else if ((i + 1) % 5 == 0)
-				model_sphere = glm::scale(model_sphere, glm::vec3(0.9f));
+			if (i <= 1)
+				model_sphere = glm::scale(model_sphere, glm::vec3(sphereSize[0]));
+			else if (i <= 3)
+				model_sphere = glm::scale(model_sphere, glm::vec3(sphereSize[1]));
+			else if (i <= 5)
+				model_sphere = glm::scale(model_sphere, glm::vec3(sphereSize[0]));
 			else
-				model_sphere = glm::scale(model_sphere, glm::vec3(1.2f));
+				model_sphere = glm::scale(model_sphere, glm::vec3(sphereSize[3]));
 			sphereShader.setMat4("model", model_sphere);
 			//glPointSize(5);
 			//glDrawElements(GL_POINTS, X_segments * Y_segments * 6, GL_UNSIGNED_INT, 0);
@@ -455,6 +536,15 @@ void processInput(GLFWwindow* window, glm::vec3* cubeRotation, size_t& taktFlag)
 		camera.ProcessKeyboard(LEFT, deltaTime);
 	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
 		camera.ProcessKeyboard(RIGHT, deltaTime);
+	if ((glfwGetKey(window, GLFW_KEY_1) && (taktFlag > 1100)) == GLFW_PRESS) {
+		ChangeDierction(cubeRotation, 0);
+		taktFlag = 0;
+	}
+	if ((glfwGetKey(window, GLFW_KEY_2) && (taktFlag > 1100)) == GLFW_PRESS) {
+		ChangeDierction(cubeRotation, 1);
+		taktFlag = 0;
+	}
+	taktFlag++;
 }
 
 void mouse_callback(GLFWwindow* window, double xpos, double ypos)
@@ -510,4 +600,89 @@ unsigned int loadTexture(const char* path)
 		stbi_image_free(data);
 	}
 	return textureID;
+}
+
+void boxMove(glm::vec3& cubePositions, glm::vec3& cubeRotation, glm::vec3* cubeLightPos) {
+
+	if ((cubePositions.x + 0.5f + cubeRotation.x >= cubeLightPos[0].x) || (cubePositions.x + 0.5f + cubeRotation.x <= cubeLightPos[1].x))
+	{
+		cubeRotation.x = -cubeRotation.x;
+
+	}
+	if ((cubePositions.y + 0.5f + cubeRotation.y >= cubeLightPos[0].y) || (cubePositions.y + 0.5f + cubeRotation.y <= cubeLightPos[1].y))
+	{
+		cubeRotation.y = -cubeRotation.y;
+	}
+	if ((cubePositions.z + 0.5f + cubeRotation.z >= cubeLightPos[0].z) || (cubePositions.z + 0.5f + cubeRotation.z <= cubeLightPos[1].z))
+	{
+		cubeRotation.z = -cubeRotation.z;
+	}
+	cubePositions.x += cubeRotation.x;
+	cubePositions.y += cubeRotation.y;
+	cubePositions.z += cubeRotation.z;
+}
+
+void sphereMove(glm::vec3& spherePositions, glm::vec3& sphereRotation, glm::vec3* cubeLightPos, float* sphereSize, size_t sphereNumber) {
+	float rad = sphereSize[sphereNumber / 2];
+	if ((spherePositions.x + rad + sphereRotation.x >= cubeLightPos[0].x) || (spherePositions.x + rad + sphereRotation.x <= cubeLightPos[1].x))
+	{
+		sphereRotation.x = -sphereRotation.x;
+
+	}
+	if ((spherePositions.y + rad + sphereRotation.y >= cubeLightPos[0].y) || (spherePositions.y + rad + sphereRotation.y <= cubeLightPos[1].y))
+	{
+		sphereRotation.y = -sphereRotation.y;
+	}
+	if ((spherePositions.z + rad + sphereRotation.z >= cubeLightPos[0].z) || (spherePositions.z + rad + sphereRotation.z <= cubeLightPos[1].z))
+	{
+		sphereRotation.z = -sphereRotation.z;
+	}
+	spherePositions.x += sphereRotation.x;
+	spherePositions.y += sphereRotation.y;
+	spherePositions.z += sphereRotation.z;
+}
+
+bool boxCollision(glm::vec3* cubePositions, size_t curCubeId, glm::vec3* cubeRotation, glm::vec3* spherePositions, glm::vec3* sphereRotation, float* sphereSize) {
+	bool res = false;
+	for (size_t i = curCubeId + 1; i < CUBS_COUNT; i++) {
+		if (abs((cubePositions[curCubeId].x) - (cubePositions[i].x)) <= 1.0f)
+			if (abs((cubePositions[curCubeId].y) - (cubePositions[i].y)) <= 1.0f)
+				if (abs((cubePositions[curCubeId].z) - (cubePositions[i].z)) <= 1.0f)
+				{
+					ChangeDierction(cubeRotation, curCubeId);
+					ChangeDierction(cubeRotation, i);
+					return true;
+				}
+	}
+	for (size_t i = 0; i < SPHERE_COUNT; i++) {
+		if (abs((cubePositions[curCubeId].x) + 0.5f - (spherePositions[i].x)) <= 0.5f + sphereSize[i / 2])
+			if (abs((cubePositions[curCubeId].y) + 0.5f - (spherePositions[i].y)) <= 0.5f + sphereSize[i / 2])
+				if (abs((cubePositions[curCubeId].z) + 0.5f - (spherePositions[i].z)) <= 0.5f + sphereSize[i / 2])
+				{
+					ChangeDierction(cubeRotation, curCubeId);
+					ChangeDierction(sphereRotation, i);
+					return true;
+				}
+	}
+	return false;
+}
+
+bool sphereCollision(glm::vec3* spherePositions, size_t curSphereId, glm::vec3* sphereRotation, float* sphereSize) {
+	for (size_t i = curSphereId + 1; i < SPHERE_COUNT; i++) {
+		if (abs((spherePositions[curSphereId].x) - (spherePositions[i].x)) <= (sphereSize[curSphereId / 2] + sphereSize[curSphereId / 2]))
+			if (abs((spherePositions[curSphereId].y) - (spherePositions[i].y)) <= (sphereSize[curSphereId / 2] + sphereSize[curSphereId / 2]))
+				if (abs((spherePositions[curSphereId].z) - (spherePositions[i].z)) <= (sphereSize[curSphereId / 2] + sphereSize[curSphereId / 2]))
+				{
+					ChangeDierction(sphereRotation, curSphereId);
+					ChangeDierction(sphereRotation, i);
+					return true;
+				}
+	}
+	return false;
+}
+
+void ChangeDierction(glm::vec3* cubeRotation, size_t cubeNumber) {
+	cubeRotation[cubeNumber].x = -cubeRotation[cubeNumber].x;
+	cubeRotation[cubeNumber].y = -cubeRotation[cubeNumber].y;
+	cubeRotation[cubeNumber].z = -cubeRotation[cubeNumber].z;
 }
